9afc428cdc8d3731357d487b122a282b26fdf6b2
Victor <vjovict@gmail.com>
2021-02-28 18:25:36 +0100

    update page 404

--- src/layouts/base.tsx
+++ src/layouts/base.tsx
@@ -3,10 +3,10 @@ import Header from "../components/header"
 
 const BaseLayout: React.FC<{}> = ({ children }) => {
   return (
-    <>
+    <div className="wrapper">
       <Header layout="base" />
       <div className="container">{children}</div>
-    </>
+    </div>
   )
 }
 
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,13 +3,13 @@ import { Link } from "gatsby"
 
 const NotFound: React.FC<{}> = () => {
   return (
-    <>
+    <div className="not-found">
       <h1>404</h1>
       <p>
-        Page does not exist. <Link to="/">Go home</Link>
+        We've got nothing here. <Link to="/">Go home</Link>
       </p>
-    </>
+    </div>
   )
 }
 
-export default NotFound
+export { NotFound as default }
