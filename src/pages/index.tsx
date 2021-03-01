115374770bf6c04347f64314ecdd7ce851d9417e
Victor <vjovict@gmail.com>
2021-03-01 23:30:19 +0100

    update home page-title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,38 @@ import { graphql, PageProps } from "gatsby"
 
 import SEO from "../components/seo"
 import PageTitle from "../components/page-title"
+import Icon from "../components/icon"
 
-type Props = PageProps & {
-  data: {
-    allFile: {
-      nodes: {
-        name: string
-        publicURL: string
-        relativeDirectory: string
-      }[]
-    }
+type DataProps = {
+  allFile: {
+    nodes: {
+      name: string
+      publicURL: string
+      relativeDirectory: string
+    }[]
   }
 }
 
-const Index: React.FC<Props> = ({ ...props }) => {
+const Index: React.FC<PageProps<DataProps>> = ({ data }) => {
   return (
     <>
       <SEO title="Friends List" />
-      <PageTitle title="Friends List" extra={<div></div>} />
+      <PageTitle
+        title="Friends List"
+        extra={
+          <div className="landing-extra">
+            <Icon name="search" directory="icons" />
+            <div className="sort-items">
+              <div className="view">
+                Sort by: <b>Newest First</b>
+              </div>
+              <div className="divider" />
+              <Icon name="dropdown" directory="icons" />
+            </div>
+            <Icon name="filter" directory="icons" />
+          </div>
+        }
+      />
     </>
   )
 }
