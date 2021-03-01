1109b92ea358da0a8a757d3d7d05a9286062003b
Victor <vjovict@gmail.com>
2021-03-01 18:35:34 +0100

    added seo to 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import SEO from "../components/seo"
+
 const NotFound: React.FC<{}> = () => {
   return (
     <div className="not-found">
+      <SEO title="Not Found" />
       <h1>404</h1>
       <p>
         We've got nothing here. <Link to="/">Go home</Link>
